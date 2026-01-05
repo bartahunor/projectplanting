56a89483ec11dd6ed36c47b7efb66c3cf4b37c1e
bartahunor <barta.hunor.attila@diak.szbi-pg.hu>
2026-01-05 11:15:16 +0100

    kezdés

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-sgsgs
+async function includeHTML(id, file) {  
+  const response = await fetch(file);
+  if (response.ok) {
+    document.getElementById(id).innerHTML = await response.text();
+  } else {
+    console.error(`Nem sikerült betölteni: ${file}`);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  // ✅ 1. Include HTML-ek betöltése (meg kell várni őket!)
+  await includeHTML("header", "pieces/header.html");
+});
