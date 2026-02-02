4578b3f84c72f13d4001a4fd2bfe343ea39a2c67
keserufruzsina <keseru.fruzsina@diak.szbi-pg.hu>
2026-02-02 08:19:11 +0100

    gomb/lista

--- public/main.js
+++ public/main.js
@@ -970,3 +970,11 @@ function mapToString(agyasokMap) {
   
   return result;
 }
+
+//FRUZSI-lista
+
+
+//FRUZSI-visszagomb
+function goHome() {
+    window.location.href = "kezdooldal.html"; // cseréld a főoldalad URL-jére
+}
